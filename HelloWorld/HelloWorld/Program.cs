1ce68096a41afd55c7f8970b35d180f9556ed780
Ben Stutzman <tysorx89@gmail.com>
2020-06-19 13:37:20 -0400

    test project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello, {name} on {date:d} at {date:t}!");
+            string output = "";
+            for (int i = 0; i < 10; i++)
+            {
+                output += name;
+                Console.WriteLine(output);
+            }
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
