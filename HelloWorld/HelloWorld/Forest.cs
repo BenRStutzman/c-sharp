47ab714f11b9098ee19f345ec7674676767ad85e
BenRStutzman <tysorx89@gmail.com>
2020-09-13 13:07:11 -0400

    Add new projects

---
+++ HelloWorld/HelloWorld/Forest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicClasses
+{
+    class Forest
+    {
+        public string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int trees;
+        public int Trees
+        {
+            get { return trees; }
+            set { trees = value; }
+        }
+        public int age;
+        public string biome;
+        public string Biome
+        {
+            get { return biome; }
+            set
+            {
+                string[] options = { "Tropical", "Temperate", "Boreal" };
+                if (options.Contains(value))
+                {
+                    Console.WriteLine("It's valid.");
+                    biome = value;
+                }
+                else
+                {
+                    biome = "Unknown";
+                }
+            }
+        }
+
+    }
+
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,24 +1,19 @@
 ﻿using System;
-using System.Xml;
 
-namespace HelloWorld
+namespace BasicClasses
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nWhat is your name? ");
-            var name = Console.ReadLine();
-            var date = DateTime.Now;
-            Console.WriteLine($"Hello, {name} on {date:d} at {date:t}!");
-            string output = "";
-            for (int i = 0; i < 10; i++)
-            {
-                output += name;
-                Console.WriteLine(output);
-            }
-            Console.Write("\nPress any key to exit...");
-            Console.ReadKey(true);
+            Forest f = new Forest();
+            f.Name = "Congo";
+            f.Trees = 0;
+            f.age = 0;
+            f.Biome = "Neptune";
+            Console.WriteLine(f.Biome);
+
+            Console.WriteLine(f.Name);
         }
     }
 }
